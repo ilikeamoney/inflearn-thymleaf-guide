fa2c5720c87b6cffc91067e5b354657895727706
ilikeamoney <2721000@naver.com>
2023-03-19 20:33:57 +0900

    thymleaf guide

---
+++ src/test/java/hello/thymeleafbasic/HellothymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymeleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellothymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
